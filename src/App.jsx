8dbdaae031fcc5459cf0c723ea023bdf1f481698
Viktor <victorharasymiv97@gmail.com>
2025-02-21 17:14:00 +0100

    all load

--- src/App.jsx
+++ src/App.jsx
@@ -51,27 +51,6 @@ function App() {
       neutral: 0
     });
   };
-  
-
-
-  // const [clicks, setClicks] = useState(() => {
-  //     // Зчитуємо значення за ключем
-  //     const savedClicks = window.localStorage.getItem("saved-clicks");
-    
-  //     // Якщо там щось є, повертаємо це
-  //     // значення як початкове значення стану
-  //     if (savedClicks !== null) {
-  //       return +savedClicks;
-  //     }
-    
-  //     // У протилежному випадку повертаємо
-  //     // яке-небудь значення за замовчуванням
-  //     return 0;
-  //   });
-
-  // useEffect(() => {
-  //   window.localStorage.setItem("saved-clicks", clicks);
-  // }, [clicks]);
 
   return (
     <>
